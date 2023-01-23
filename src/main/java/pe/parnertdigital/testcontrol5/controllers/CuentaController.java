4703d193fbbc0b1391309a540798a65c61224426
davidyallerco <david.yallerco@gmail.com>
2023-01-23 18:28:18 -0500

    v1 inicial con controlador 2

--- src/main/java/pe/parnertdigital/testcontrol5/controllers/CuentaController.java
+++ src/main/java/pe/parnertdigital/testcontrol5/controllers/CuentaController.java
@@ -3,13 +3,17 @@ package pe.parnertdigital.testcontrol5.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.*;
 import pe.parnertdigital.testcontrol5.models.Cuenta;
+import pe.parnertdigital.testcontrol5.models.TransaccionDto;
 import pe.parnertdigital.testcontrol5.services.CuentaService;
+
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 
 @Controller
@@ -23,5 +27,16 @@ public class CuentaController {
     @ResponseStatus(HttpStatus.OK)
     public Cuenta detalle(@PathVariable Long id){
         return cuentaService.buscarPorId(id);
+    }
+
+    @PostMapping("/transferir")
+    public ResponseEntity<?> transferir(@RequestBody TransaccionDto dto){
+        cuentaService.tranferir(dto.getCuentaOrigenId(), dto.getCuentaDestinoId(), dto.getMonto(), dto.getBancoId());
+        Map<String, Object> response = new HashMap<>();
+        response.put("date", LocalDate.now().toString());
+        response.put("status","OK");
+        response.put("mensaje", "Transferencia realizada con exito!");
+        response.put("transaccion", dto);
+        return ResponseEntity.ok(response);
     }
 }
