12abeb77d1678d1ee145bded6cec62609fc0870f
davidyallerco <david.yallerco@gmail.com>
2023-01-24 10:44:51 -0500

    v5 metodo testTransferir

--- src/test/java/pe/parnertdigital/testcontrol5/controllers/CuentaControllerTest.java
+++ src/test/java/pe/parnertdigital/testcontrol5/controllers/CuentaControllerTest.java
@@ -1,5 +1,8 @@
 package pe.parnertdigital.testcontrol5.controllers;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -8,8 +11,11 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import pe.parnertdigital.testcontrol5.Datos;
+import pe.parnertdigital.testcontrol5.models.TransaccionDto;
 import pe.parnertdigital.testcontrol5.services.CuentaService;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.NoSuchElementException;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -28,6 +34,13 @@ class CuentaControllerTest {
     @MockBean
     private CuentaService cuentaService;
 
+    ObjectMapper objectMapper;
+
+    @BeforeEach
+    void serUp(){
+        objectMapper = new ObjectMapper();
+    }
+
     @Test
     void testDetalle() throws Exception {
         //Given
@@ -42,5 +55,26 @@ class CuentaControllerTest {
                 .andExpect(jsonPath("$.persona").value("David"))
                 .andExpect(jsonPath("$.saldo").value("1000"));
         verify(cuentaService).buscarPorId(1L);
+    }
+
+    @Test
+    void testTransferir() throws Exception, JsonProcessingException {
+        TransaccionDto dto = new TransaccionDto();
+        dto.setCuentaOrigenId(1L);
+        dto.setCuentaDestinoId(2L);
+        dto.setMonto(new BigDecimal("100"));
+        dto.setBancoId(1L);
+
+        //when
+        mvc.perform(post("/api/cuentas/transferir")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(dto)))
+
+        //Then
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.date").value(LocalDate.now().toString()))
+                .andExpect(jsonPath("$.mensaje").value("Transferencia realizada con exito!"))
+                .andExpect(jsonPath("$.transaccion.cuentaOrigenId").value(1L));
     }
 }
